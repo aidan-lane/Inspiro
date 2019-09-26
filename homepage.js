c9d28c436faa270ca907a5654f9bd17d43728f20
Aidan Lane <aidanlane7@gmail.com>
2019-09-25 22:09:46 -0400

    gets data from api

---
+++ homepage.js
@@ -0,0 +1,17 @@
+// handle html inject setup
+const parentDiv = document.getElementById("lga");
+var injection = document.createElement("div");
+injection.innerHTML = "Sorry, no quote could be retrieved...";
+parentDiv.appendChild(injection);
+
+// send quote request to REST API
+const URL = "https://quotes.rest/qod";
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+       var parsed = JSON.parse(this.responseText);
+       injection.innerHTML = parsed.contents.quotes[0].quote;
+    } 
+}
+request.open("GET", URL, true);
+request.send();
